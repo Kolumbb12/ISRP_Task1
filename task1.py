d1c01d023bad8e4145e8d9a1f8e15b42222acb50
Kolumbb12 <kolumbb1207@gmail.com>
2023-09-07 17:04:05 +0600

    v2.0

--- task1.py
+++ task1.py
@@ -1,11 +1,11 @@
-def math_func(a, b):
-    if math_operation == "+":
+def math_func(a, b, operation):
+    if operation == "+":
         return a + b
-    elif math_operation == "-":
+    elif operation == "-":
         return a - b
-    elif math_operation == "*":
+    elif operation == "*":
         return a * b
-    elif math_operation == "/":
+    elif operation == "/":
         if b != 0:
             return a / b
         else:
@@ -14,12 +14,17 @@ def math_func(a, b):
         return "Ошибка: недопустимая операция"
 
 
-num1 = float(input('Введите первое число: '))
-num2 = float(input('Введите второе число: '))
-math_operation = input('Введите математическую операцию (+, -, *, /): ')
+while True:
+    num1 = float(input('Введите первое число: '))
+    num2 = float(input('Введите второе число: '))
+    math_operation = input('Введите математическую операцию (+, -, *, /): ')
 
+    print(f'Результат: {math_func(num1, num2, math_operation)}')
 
-print(f'Результат: {math_func(num1, num2)}')
+    again = input('Желаете продолжить (да/нет)? ')
+    if again.lower() != 'да':
+        break
+
 
 
 
